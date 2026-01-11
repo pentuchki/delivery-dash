20400f366235b2feecf82e11f2912c04f9f713c3
pentuchki <arslan.umutcan@gmail.com>
2026-01-11 23:27:26 +0300

    Added delivery mechanics to related assets

--- Assets/Delivery.cs
+++ Assets/Delivery.cs
@@ -2,15 +2,19 @@ using UnityEngine;
 
 public class Delivery : MonoBehaviour
 {
+    bool hasPackage;
+
     void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.CompareTag("Package"))
         {
-            Debug.Log("Picked up package");   
+            Debug.Log("Picked up package");
+            hasPackage = true;   
         }
-        if(collision.CompareTag("Customer"))
+        if(collision.CompareTag("Customer") && hasPackage)
         {
             Debug.Log("Delivered the package");
+            hasPackage = false;
         }
     }
 }
